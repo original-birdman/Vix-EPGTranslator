604d033f97fe2f7a92742bc1e80b2ffa7d8a6f24
Gordon Lack <gordon.lack@dsl.pipex.com>
2021-01-13 19:48:36 +0000

    Enlarged the translatorConfig screen. Added the (forgotten) yellow image back to translatorMain.

--- src/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/Skin_medium.py
+++ src/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/Skin_medium.py
@@ -1,4 +1,4 @@
-# Skin data for screenwidth > 1280
+# Skin data for screenwidth > 1280  
 # Only contains "class" data.  No instance data
 #
 
@@ -7,16 +7,16 @@ class MySkinData:
 # class translatorConfig bits
 #
     translatorConfig_skin = """
-        <screen position="center,center" size="818,788" backgroundColor="#20000000" title="EPG Translator Setup">
-            <ePixmap position="0,0" size="818,75" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/fhd-translatorConfig.png" alphatest="blend" zPosition="1" />
-            <ePixmap position="15,89" size="788,1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/separator.png" alphatest="off" zPosition="1" />
-            <widget name="config" position="15,90" size="788,188" itemHeight="38" scrollbarMode="showOnDemand" font="Regular;27" secondfont="Regular;27" zPosition="1" />
-            <ePixmap position="15,279" size="788,1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/separator.png" alphatest="off" zPosition="1" />
-            <ePixmap position="188,294" size="27,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/fhd-green.png" alphatest="blend" zPosition="1" />
-            <ePixmap position="525,294" size="27,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/fhd-red.png" alphatest="blend" zPosition="1" />
-            <eLabel position="225,293" size="270,30" font="Regular;27" halign="left" text="Save" transparent="1" zPosition="1" />
-            <eLabel position="563,293" size="270,30" font="Regular;27" halign="left" text="Cancel" transparent="1" zPosition="1" />
-            <widget name="flag" position="264,410" size="288,288" alphatest="blend" zPosition="1" />
+        <screen position="center,center" size="1218,788" backgroundColor="#20000000" title="EPG Translator Setup">
+            <ePixmap position="200,0" size="1018,75" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/fhd-translatorConfig.png" alphatest="blend" zPosition="1" />
+            <ePixmap position="215,89" size="988,1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/separator.png" alphatest="off" zPosition="1" />
+            <widget name="config" position="15,90" size="1188,268" itemHeight="57" scrollbarMode="showOnDemand" font="Regular;36" secondfont="Regular;36" zPosition="1" />
+            <ePixmap position="215,369" size="988,1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/separator.png" alphatest="off" zPosition="1" />
+            <ePixmap position="318,384" size="27,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/fhd-green.png" alphatest="blend" zPosition="1" />
+            <ePixmap position="795,384" size="27,27" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/fhd-red.png" alphatest="blend" zPosition="1" />
+            <eLabel position="355,383" size="270,40" font="Regular;36" halign="left" text="Save" transparent="1" zPosition="1" />
+            <eLabel position="833,383" size="270,40" font="Regular;36" halign="left" text="Cancel" transparent="1" zPosition="1" />
+            <widget name="flag" position="464,450" size="288,288" alphatest="blend" zPosition="1" />
         </screen>
         """
 
@@ -28,8 +28,9 @@ class MySkinData:
         <screen position="center,120" size="1500,915" title="EPG Translator">
             <ePixmap position="0,0" size="1500,75" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/fhd-translator.png" alphatest="blend" zPosition="1" />
             <ePixmap position="15,9" size="54,54" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/fhd-blue.png" alphatest="blend" zPosition="2" />
-            <widget name="label" position="51,9" size="300,30" font="Regular;24" foregroundColor="#697178" backgroundColor="#FFFFFF" halign="left" transparent="1" zPosition="2" />
-            <widget name="label2" position="51,39" size="300,30" font="Regular;24" foregroundColor="#697178" backgroundColor="#FFFFFF" halign="left" transparent="1" zPosition="2" />
+            <ePixmap position="15,39" size="54,54" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/fhd-yellow.png" alphatest="blend" zPosition="2" />
+            <widget name="label" position="51,9" size="300,30" font="Regular;24" foregroundColor="black" backgroundColor="#FFFFFF" halign="left" transparent="1" zPosition="2" />
+            <widget name="label2" position="51,39" size="300,30" font="Regular;24" foregroundColor="black" backgroundColor="#FFFFFF" halign="left" transparent="1" zPosition="2" />
             <widget render="Label" source="global.CurrentTime" position="1110,0" size="360,75" font="Regular;36" foregroundColor="#697178" backgroundColor="#FFFFFF" halign="right" valign="center" zPosition="2">
             <convert type="ClockToText">Format:%H:%M:%S</convert>
             </widget>
--- src/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/Skin_small.py
+++ src/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/Skin_small.py
@@ -7,16 +7,16 @@ class MySkinData:
 # class translatorConfig bits
 #
     translatorConfig_skin = """
-        <screen position="center,center" size="545,525" backgroundColor="#20000000" title="EPG Translator Setup">
-            <ePixmap position="0,0" size="545,50" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/translatorConfig.png" alphatest="blend" zPosition="1" />
-            <ePixmap position="10,59" size="525,1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/separator.png" alphatest="off" zPosition="1" />
-            <widget name="config" position="10,60" size="525,125" itemHeight="25" scrollbarMode="showOnDemand" font="Regular;18" secondfont="Regular;18" zPosition="1" />
-            <ePixmap position="10,186" size="525,1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/separator.png" alphatest="off" zPosition="1" />
-            <ePixmap position="125,196" size="18,18" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/green.png" alphatest="blend" zPosition="1" />
-            <ePixmap position="350,196" size="18,18" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/red.png" alphatest="blend" zPosition="1" />
-            <eLabel position="150,195" size="180,20" font="Regular;18" halign="left" text="Save" transparent="1" zPosition="1" />
-            <eLabel position="375,195" size="180,20" font="Regular;18" halign="left" text="Cancel" transparent="1" zPosition="1" />
-            <widget name="flag" position="128,225" size="288,288" alphatest="blend" zPosition="1" />
+        <screen position="center,center" size="811,525" backgroundColor="#20000000" title="EPG Translator Setup">
+            <ePixmap position="133,0" size="811,50" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/translatorConfig.png" alphatest="blend" zPosition="1" />
+            <ePixmap position="143,59" size="658,1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/separator.png" alphatest="off" zPosition="1" />
+            <widget name="config" position="10,60" size="658,182" itemHeight="38" scrollbarMode="showOnDemand" font="Regular;24" secondfont="Regular;24" zPosition="1" />
+            <ePixmap position="143,244" size="658,1" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/separator.png" alphatest="off" zPosition="1" />
+            <ePixmap position="210,254" size="18,18" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/green.png" alphatest="blend" zPosition="1" />
+            <ePixmap position="525,254" size="18,18" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/red.png" alphatest="blend" zPosition="1" />
+            <eLabel position="235,253" size="180,26" font="Regular;24" halign="left" text="Save" transparent="1" zPosition="1" />
+            <eLabel position="550,253" size="180,26" font="Regular;24" halign="left" text="Cancel" transparent="1" zPosition="1" />
+            <widget name="flag" position="261,250" size="288,288" alphatest="blend" zPosition="1" />
         </screen>
         """
 
@@ -28,8 +28,9 @@ class MySkinData:
         <screen position="center,80" size="1000,610" title="EPG Translator">
             <ePixmap position="0,0" size="1000,50" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/translator.png" alphatest="blend" zPosition="1" />
             <ePixmap position="10,6" size="36,36" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/blue.png" alphatest="blend" zPosition="2" />
-            <widget name="label" position="34,6" size="200,20" font="Regular;16" foregroundColor="#697178" backgroundColor="#FFFFFF" halign="left" transparent="1" zPosition="2" />
-            <widget name="label2" position="34,26" size="200,20" font="Regular;16" foregroundColor="#697178" backgroundColor="#FFFFFF" halign="left" transparent="1" zPosition="2" />
+            <ePixmap position="10,26" size="36,36" pixmap="/usr/lib/enigma2/python/Plugins/Extensions/EPGTranslator/pic/buttons/yellow.png" alphatest="blend" zPosition="2" />
+            <widget name="label" position="34,6" size="200,20" font="Regular;16" foregroundColor="black" backgroundColor="#FFFFFF" halign="left" transparent="1" zPosition="2" />
+            <widget name="label2" position="34,26" size="200,20" font="Regular;16" foregroundColor="black" backgroundColor="#FFFFFF" halign="left" transparent="1" zPosition="2" />
             <widget render="Label" source="global.CurrentTime" position="740,0" size="240,50" font="Regular;24" foregroundColor="#697178" backgroundColor="#FFFFFF" halign="right" valign="center" zPosition="2">
             <convert type="ClockToText">Format:%H:%M:%S</convert>
             </widget>
